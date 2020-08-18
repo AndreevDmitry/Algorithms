970f5fd4f47d74d0407075098e9b1b68d8347892
Dmitry Andreev <d.andreev@omprussia.ru>
2020-08-18 08:53:03 +0300

    Make pylint happy
    Add description for file and method, add new line at the end of file.

--- compare3args.py
+++ compare3args.py
@@ -1,9 +1,12 @@
+"""
+1.1.3 Write a program that takes three integer command-line arguments
+and prints equal if all three are equal, and not equal otherwise.
+"""
+
 import sys
 
-#1.1.3 Write a program that takes three integer command-line arguments 
-# and prints equal if all three are equal, and not equal otherwise.
-
 def compare3args(arg1, arg2, arg3):
+    """Return "Equal" if all 3 args are equal, otherwise "Not Equal" """
     if (arg1 == arg2 and arg2 == arg3):
         print("Equal")
     else:
