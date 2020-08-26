15303b703c168ff184d02be21437b471decb11a7
Dmitry Andreev <d.andreev@omprussia.ru>
2020-08-26 09:33:39 +0300

    Update for search in array passed by user.

--- binary_search.py
+++ binary_search.py
@@ -1,21 +1,22 @@
 """
 Binary search implementation
+Example:$ python3 binary_search.py 1 1,2,3,4
+0
 """
 
 import sys
 
-def binary_search(start_value, end_value, item):
+def binary_search(item, array):
     """
-    Creates sorted array and execute binary search
-    of passed item. Retuns position of item or
+    Execute binary search of passed item
+    in passed array. Returns position of item or
     None if not exists
     """
-    array_list = list(range(start_value, end_value+1))
-    high = len(array_list) - 1
+    high = len(array) - 1
     low = 0
     while low <= high:
         mid = int((low + high)/2)
-        guess = array_list[mid]
+        guess = array[mid]
         if guess > item:
             high = mid - 1
         elif guess < item:
@@ -25,15 +26,15 @@ def binary_search(start_value, end_value, item):
 
     return None
 
-print(binary_search(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])))
+print(binary_search(int(sys.argv[1]), list(map(int, sys.argv[2].split(',')))))
 
 def test():
     """Some tests"""
-    assert binary_search(1, 5, 1) == 0
-    assert binary_search(0, 0, 0) == 0
-    assert binary_search(-100, 100, 100) == 200
-    assert binary_search(-100, 100, 300) is None
-    assert binary_search(-1, 1, 0) == 1
+    assert binary_search(1, [1, 2, 3, 4]) == 0
+    assert binary_search(0, [0]) == 0
+    assert binary_search(4, [1, 2, 3, 4]) == 3
+    assert binary_search(5, [1, 2, 3, 4]) is None
+    assert binary_search(0, [-1, 0, 1]) == 1
     print("-----------------Tests are PASSED-----------------------")
 
 if __name__ == "__main__":
